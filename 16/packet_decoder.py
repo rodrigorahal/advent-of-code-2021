e07582aece02516d75892f39a4e28919d5d0a665
Rodrigo Rahal <rrrahal19@gmail.com>
2021-12-16 13:21:12 -0300

    refactor day 16

--- 16/packet_decoder.py
+++ 16/packet_decoder.py
@@ -1,5 +1,16 @@
 import fileinput
 from math import prod
+from dataclasses import dataclass
+
+
+@dataclass
+class Packet:
+    version: int
+    type_id: int
+    content: str
+    endptr: int
+    literal: int
+    subpackets: list
 
 
 def parse():
@@ -13,9 +24,12 @@ def convert(message):
     return bits
 
 
-def decode_packet(bits, i, versions):
-    value = None
+def decode(bits):
+    packet = decode_packet(bits, i=0)
+    return packet
 
+
+def decode_packet(bits, i):
     version = int(bits[i : i + 3], 2)
     i += 3
 
@@ -23,6 +37,7 @@ def decode_packet(bits, i, versions):
     i += 3
 
     if type_id == 4:
+        startptr = i
         digits = ""
         is_last = "1"
         while is_last == "1":
@@ -30,61 +45,77 @@ def decode_packet(bits, i, versions):
             i += 1
             digits += bits[i : i + 4]
             i += 4
-        value = int(digits, 2)
+        packet = Packet(version, type_id, bits[startptr:i], i, int(digits, 2), [])
 
     else:
         length_type_id = bits[i]
         i += 1
 
-        if length_type_id == "0":  # total length
+        if length_type_id == "0":
+            startptr = i
             total_length = int(bits[i : i + 15], 2)
             i += 15
-            start = i
-            values = []
-            while i < start + total_length:
-                versions, i, subvalue = decode_packet(bits, i, versions)
-                values.append(subvalue)
+            subpackets = []
+            while i < startptr + total_length + 15:
+                subpacket = decode_packet(bits, i)
+                subpackets.append(subpacket)
+                i = subpacket.endptr
+            packet = Packet(version, type_id, bits[startptr:i], i, None, subpackets)
 
-        elif length_type_id == "1":  # num of sub-packets
-            nsubpacktes = int(bits[i : i + 11], 2)
+        elif length_type_id == "1":
+            startptr = i
+            n_subpackets = int(bits[i : i + 11], 2)
             i += 11
-            n = 0
-            values = []
-            while n < nsubpacktes:
-                versions, i, subvalue = decode_packet(bits, i, versions)
-                values.append(subvalue)
-                n += 1
+            subpackets = []
+            for _ in range(n_subpackets):
+                subpacket = decode_packet(bits, i)
+                i = subpacket.endptr
+                subpackets.append(subpacket)
+            packet = Packet(version, type_id, bits[startptr:i], i, None, subpackets)
 
-        if type_id == 0:
-            value = sum(values)
-        elif type_id == 1:
-            value = prod(values)
-        elif type_id == 2:
-            value = min(values)
-        elif type_id == 3:
-            value = max(values)
-        elif type_id == 5:
-            value = 1 if values[0] > values[1] else 0
-        elif type_id == 6:
-            value = 1 if values[0] < values[1] else 0
-        elif type_id == 7:
-            value = 1 if values[0] == values[1] else 0
-
-    return versions + version, i, value
+    return packet
 
 
-def decode(bits):
-    versions, _, value = decode_packet(bits, i=0, versions=0)
-    return versions, value
+def versions(packet: Packet):
+    versions = 0
+    stack = [packet]
+
+    while stack:
+        packet = stack.pop()
+        versions += packet.version
+
+        for subpacket in packet.subpackets:
+            stack.append(subpacket)
+    return versions
+
+
+def eval(packet: Packet):
+    if packet.type_id == 4:
+        return packet.literal
+
+    if packet.type_id == 0:
+        return sum(eval(subpacket) for subpacket in packet.subpackets)
+    if packet.type_id == 1:
+        return prod(eval(subpacket) for subpacket in packet.subpackets)
+    if packet.type_id == 2:
+        return min(eval(subpacket) for subpacket in packet.subpackets)
+    if packet.type_id == 3:
+        return max(eval(subpacket) for subpacket in packet.subpackets)
+    if packet.type_id == 5:
+        return 1 if eval(packet.subpackets[0]) > eval(packet.subpackets[1]) else 0
+    if packet.type_id == 6:
+        return 1 if eval(packet.subpackets[0]) < eval(packet.subpackets[1]) else 0
+    if packet.type_id == 7:
+        return 1 if eval(packet.subpackets[0]) == eval(packet.subpackets[1]) else 0
 
 
 def main():
     messages = parse()
 
     for message in messages:
-        versions, value = decode(convert(message))
-        print(f"versions: {versions}")
-        print(f"value: {value}")
+        packet = decode(convert(message))
+        print(f"Part 1: {versions(packet)}")
+        print(f"Part 2: {eval(packet)}")
 
 
 if __name__ == "__main__":
